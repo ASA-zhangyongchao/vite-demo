cd3b9ec09aeb25344583a2cbd1145aca6852202f
张永超 <162996163+ASA-zhangyongchao@users.noreply.github.com>
2025-02-19 03:40:15 +0000

    base 路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 张永超 162996163+ASA-zhangyongchao@users.noreply.github.com
+ * @Date: 2025-02-19 03:15:13
+ * @LastEditors: 张永超 162996163+ASA-zhangyongchao@users.noreply.github.com
+ * @LastEditTime: 2025-02-19 03:39:17
+ * @FilePath: /vite-demo/vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -10,6 +18,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === "production" ? "/wx-web-demo/" : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
