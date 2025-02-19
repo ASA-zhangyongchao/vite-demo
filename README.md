eda8031cc79e663453ba3338a9497b67dbca86fc
张永超 <162996163+ASA-zhangyongchao@users.noreply.github.com>
2025-02-19 09:43:16 +0000

    base 地址变更

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: 张永超 162996163+ASA-zhangyongchao@users.noreply.github.com
+ * @Date: 2025-02-19 03:15:13
+ * @LastEditors: 张永超 162996163+ASA-zhangyongchao@users.noreply.github.com
+ * @LastEditTime: 2025-02-19 03:52:26
+ * @FilePath: /vite-demo/README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # vite-project
 
 This template should help get you started developing with Vue 3 in Vite.
@@ -30,4 +38,10 @@ npm run dev
 
 ```sh
 npm run build
+```
+
+# 自动部署 github pages
+```sh 
+提交前 执行
+npm run deploy
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 张永超 162996163+ASA-zhangyongchao@users.noreply.github.com
  * @Date: 2025-02-19 03:15:13
  * @LastEditors: 张永超 162996163+ASA-zhangyongchao@users.noreply.github.com
- * @LastEditTime: 2025-02-19 03:39:17
+ * @LastEditTime: 2025-02-19 09:42:22
  * @FilePath: /vite-demo/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,7 +18,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: process.env.NODE_ENV === "production" ? "/wx-web-demo/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vite-demo/" : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
